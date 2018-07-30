5f816e67b0acb9eb05206e43028e5606c5dc0700
Dmytro Rubizhenko <d.rubizhenko@gmail.com>
2018-07-30 10:17:10 +0300

    Finished gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -232,20 +232,13 @@ gulp.task('spriteSVG:build', function () {
 			},
 			transformData: function (data, config) {
 				data.svg.map(function (item) {
-					//change id attribute
 					item.data = item.data.replace(/id=\"([^\"]+)\"/gm, 'id="' + item.name + '-$1"');
-
-					//change id in fill attribute
 					item.data = item.data.replace(/fill=\"url\(\#([^\"]+)\)\"/gm, 'fill="url(#' + item.name + '-$1)"');
-
-					//change id in mask attribute
 					item.data = item.data.replace(/mask=\"url\(\#([^\"]+)\)\"/gm, 'mask="url(#' + item.name + '-$1)"');
-
-					//replace double id for the symbol tag
 					item.data = item.data.replace('id="' + item.name + '-' + item.name + '"', 'id="' + item.name + '"');
 					return item;
 				});
-				return data; // modify the data and return it
+				return data;
 			}
 		}))
 		.pipe(gulp.dest('src/img/svg'));
