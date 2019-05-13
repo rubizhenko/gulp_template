db302b6317ca91f7001012e7515c61f1a4fb929a
Dmytro Rubizhenko <d.rubizhenko@gmail.com>
2019-05-13 13:05:57 +0300

    started new gulpfile

---
+++ gulpfile-old.js
@@ -0,0 +1,518 @@
+"use strict";
+
+const gulp = require("gulp"),
+  watch = require("gulp-watch"),
+  sass = require("gulp-sass"),
+  cssmin = require("gulp-minify-css"),
+  autoprefixer = require("autoprefixer"),
+  mqpacker = require("css-mqpacker"),
+  postcss = require("gulp-postcss"),
+  uglify = require("gulp-uglify"),
+  sourcemaps = require("gulp-sourcemaps"),
+  include = require("gulp-include"),
+  htmlmin = require("gulp-htmlmin"),
+  imagemin = require("gulp-imagemin"),
+  pngquant = require("imagemin-pngquant"),
+  rimraf = require("rimraf"),
+  wait = require("gulp-wait"),
+  browserSync = require("browser-sync"),
+  svgSprite = require("gulp-svg-sprites"),
+  spritesmith = require("gulp.spritesmith-multi"),
+  svgo = require("gulp-svgo"),
+  iconfont = require("gulp-iconfont"),
+  iconfontCss = require("gulp-iconfont-css"),
+  reload = browserSync.reload,
+  named = require("vinyl-named"),
+  webpack = require("webpack-stream");
+
+var path = {
+  src: {
+    html: "src/*.html",
+    js: "src/js/*.js",
+    jsLib: "src/js/lib/*.js",
+    json: "src/json/*.json",
+    style: "src/style/*.sass",
+    img: "src/img/**/*.*",
+    svg: "src/svg/*.svg",
+    sprite: "src/sprite/**/*.+(jpg|jpeg|png)",
+    spriteSVG: "src/spriteSVG/*.svg",
+    svgico: "src/svgico/*.svg",
+    video: "src/video/*.*",
+    favicon: "src/favicon/*.*",
+    fonts: "src/fonts/**/*.*"
+  },
+  build: {
+    html: "build/",
+    js: "build/js/",
+    json: "build/json",
+    css: "build/css/",
+    img: "build/img/",
+    svg: "build/img/svg",
+    favicon: "build/favicon/",
+    fonts: "build/fonts/",
+    video: "build/video/"
+  },
+  deploy: {
+    html: "www/",
+    js: "www/js/",
+    json: "www/json/",
+    css: "www/css/",
+    img: "www/img/",
+    svg: "www/img/svg",
+    favicon: "www/favicon/",
+    fonts: "www/fonts/",
+    video: "www/video/"
+  },
+  watch: {
+    html: "src/**/*.html",
+    js: "src/js/**/*.js",
+    style: "src/style/**/*.+(scss|sass|css)",
+    img: "src/img/**/*.+(jpg|jpeg|png|gif|ico)",
+    sprite: "src/sprite/**/*.+(jpg|jpeg|png)",
+    spriteSVG: "src/spriteSVG/*.svg",
+    svg: "src/svg/*.svg",
+    svgico: "src/svgico/*.svg",
+    favicon: "src/**/*.*",
+    video: "src/video/*.*",
+    fonts: "src/fonts/**/*.*"
+  },
+  clean: "./build"
+};
+
+var config = {
+  server: {
+    baseDir: "./build"
+  },
+  tunnel: false,
+  host: "localhost",
+  port: 9000,
+  logPrefix: "gulper"
+};
+
+gulp.task("html:build", function() {
+  gulp
+    .src(path.src.html)
+    .pipe(include())
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(path.build.html))
+    .pipe(
+      reload({
+        stream: true
+      })
+    );
+});
+
+gulp.task("html:deploy", function() {
+  gulp
+    .src(path.src.html)
+    .pipe(include())
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(path.deploy.html));
+});
+
+gulp.task("favicon:build", function() {
+  gulp
+    .src(path.src.favicon)
+    .pipe(gulp.dest(path.build.favicon))
+    .pipe(
+      reload({
+        stream: true
+      })
+    );
+});
+gulp.task("favicon:deploy", function() {
+  gulp.src(path.src.favicon).pipe(gulp.dest(path.deploy.favicon));
+});
+
+gulp.task("json:build", function() {
+  gulp
+    .src(path.src.json)
+    .pipe(gulp.dest(path.build.json))
+    .pipe(
+      reload({
+        stream: true
+      })
+    );
+});
+gulp.task("json:deploy", function() {
+  gulp.src(path.src.json).pipe(gulp.dest(path.build.json));
+});
+gulp.task("js-lib:build", function() {
+  gulp
+    .src(path.src.jsLib)
+    .pipe(gulp.dest(path.build.js))
+    .pipe(
+      reload({
+        stream: true
+      })
+    );
+});
+gulp.task("js:build", function() {
+  gulp
+    .src(path.src.js)
+    .pipe(named())
+    .pipe(
+      webpack({
+        mode: "development",
+        devtool: "source-map",
+        module: {
+          rules: [
+            {
+              test: /\.(js)$/,
+              loader: "babel-loader",
+              exclude: /(node_modules)/,
+              query: {
+                presets: ["env"]
+              }
+            },
+            //for importing snapSVG
+            {
+              test: require.resolve("snapsvg/dist/snap.svg.js"),
+              use: "imports-loader?this=>window,fix=>module.exports=0"
+            }
+            //END: for importing snapSVG
+          ]
+        },
+        //for importing snapSVG
+        resolve: {
+          alias: {
+            snapsvg: "snapsvg/dist/snap.svg.js"
+          }
+        },
+        //END:for importing snapSVG
+        externals: {
+          jquery: "jQuery"
+        }
+      })
+    )
+    .pipe(gulp.dest(path.build.js))
+    .pipe(
+      reload({
+        stream: true
+      })
+    );
+});
+gulp.task("js:deploy", function() {
+  gulp
+    .src(path.src.js)
+    .pipe(named())
+    .pipe(
+      webpack({
+        mode: "production",
+        devtool: "source-map",
+        module: {
+          rules: [
+            {
+              test: /\.(js)$/,
+              loader: "babel-loader",
+              exclude: /(node_modules)/,
+              query: {
+                presets: ["env"]
+              }
+            },
+            //for importing snapSVG
+            {
+              test: require.resolve("snapsvg/dist/snap.svg.js"),
+              use: "imports-loader?this=>window,fix=>module.exports=0"
+            }
+            //END: for importing snapSVG
+          ]
+        },
+        //for importing snapSVG
+        resolve: {
+          alias: {
+            snapsvg: "snapsvg/dist/snap.svg.js"
+          }
+        },
+        //END:for importing snapSVG
+        externals: {
+          jquery: "jQuery"
+        }
+      })
+    )
+    .pipe(gulp.dest(path.deploy.js));
+});
+gulp.task("js-lib:deploy", function() {
+  gulp.src(path.src.jsLib).pipe(gulp.dest(path.deploy.js));
+});
+gulp.task("style:build", function() {
+  var processors = [
+    autoprefixer({
+      browsers: ["> 5%"],
+      cascade: false
+    }),
+    mqpacker({
+      sort: function(a, b) {
+        a = a.replace(/\D/g, "");
+        b = b.replace(/\D/g, "");
+        return b - a;
+        // replace this with a-b for Mobile First approach
+      }
+    })
+  ];
+  gulp
+    .src(path.src.style)
+    .pipe(sourcemaps.init({ largeFile: true }))
+    .pipe(wait(500))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss(processors))
+    .pipe(cssmin())
+    .pipe(sourcemaps.write("../maps"))
+    .pipe(gulp.dest(path.build.css))
+    .pipe(
+      reload({
+        stream: true
+      })
+    );
+});
+gulp.task("style:deploy", function() {
+  var processors = [
+    autoprefixer({
+      browsers: ["last 10 versions"],
+      cascade: false
+    }),
+    mqpacker({
+      sort: function(a, b) {
+        a = a.replace(/\D/g, "");
+        b = b.replace(/\D/g, "");
+        return b - a;
+        // replace this with a-b for Mobile First approach
+      }
+    })
+  ];
+  gulp
+    .src(path.src.style)
+    .pipe(wait(500))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss(processors))
+    .pipe(cssmin())
+    .pipe(gulp.dest(path.deploy.css));
+});
+
+gulp.task("image:build", function() {
+  gulp
+    .src(path.src.img)
+    .pipe(gulp.dest(path.build.img))
+    .pipe(
+      reload({
+        stream: true
+      })
+    );
+});
+gulp.task("image:deploy", function() {
+  gulp
+    .src(path.build.img + "/**/*.*")
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [
+          {
+            removeViewBox: false
+          }
+        ],
+        use: [pngquant()],
+        interlaced: true
+      })
+    )
+    .pipe(gulp.dest(path.deploy.img));
+});
+var options = {
+  spritesmith: function(option, sprite) {
+    option.imgName = sprite + ".png";
+    option.cssName = sprite + ".sass";
+    option.imgPath = "../img/" + sprite + ".png";
+    option.padding = 10;
+    delete option.cssTemplate;
+  }
+};
+gulp.task("sprite:build", function() {
+  var spriteData = gulp.src(path.src.sprite).pipe(spritesmith(options));
+  spriteData.img.pipe(gulp.dest(path.build.img));
+  spriteData.css.pipe(gulp.dest("src/style/libs/")).pipe(
+    reload({
+      stream: true
+    })
+  );
+});
+
+gulp.task("svg:build", function() {
+  return gulp
+    .src(path.src.svg)
+    .pipe(wait(3000))
+    .pipe(svgo())
+    .pipe(gulp.dest(path.build.svg));
+});
+gulp.task("svg:deploy", function() {
+  return gulp
+    .src(path.src.svg)
+    .pipe(svgo())
+    .pipe(gulp.dest(path.deploy.svg));
+});
+
+gulp.task("spriteSVG:build", function() {
+  return gulp
+    .src(path.src.spriteSVG)
+    .pipe(wait(3000))
+    .pipe(
+      svgSprite({
+        mode: "symbols",
+        preview: {
+          symbols: "../../preview/symbols.html"
+        },
+        selector: "%f",
+        svg: {
+          symbols: "symbols.svg"
+        },
+        transformData: function(data, config) {
+          data.svg.map(function(item) {
+            item.data = item.data.replace(
+              /id=\"([^\"]+)\"/gm,
+              'id="' + item.name + '-$1"'
+            );
+            item.data = item.data.replace(
+              /fill=\"url\(\#([^\"]+)\)\"/gm,
+              'fill="url(#' + item.name + '-$1)"'
+            );
+            item.data = item.data.replace(
+              /mask=\"url\(\#([^\"]+)\)\"/gm,
+              'mask="url(#' + item.name + '-$1)"'
+            );
+            item.data = item.data.replace(
+              'id="' + item.name + "-" + item.name + '"',
+              'id="' + item.name + '"'
+            );
+            return item;
+          });
+          return data;
+        }
+      })
+    )
+    .pipe(gulp.dest("src/img/svg"));
+});
+
+gulp.task("svg-ico:build", function() {
+  return gulp
+    .src(path.src.svgico)
+    .pipe(wait(1000))
+    .pipe(svgo())
+    .pipe(
+      iconfontCss({
+        fontName: "fico", // required
+        target: "src/style/partials/font-icons.scss",
+        targetPath: "../../style/partials/font-icons.scss",
+        fontPath: "../fonts/icons/",
+        cssClass: "fico"
+      })
+    )
+    .pipe(
+      iconfont({
+        fontName: "fico", // required
+        prependUnicode: true, // recommended option
+        formats: ["ttf", "eot", "woff", "woff2", "svg"], // default, 'woff2' and 'svg' are available
+        normalize: true,
+        fontHeight: 1001,
+        fontStyle: "normal",
+        fontWeight: "normal"
+      })
+    )
+    .pipe(gulp.dest("src/fonts/icons"));
+});
+
+gulp.task("fonts:build", function() {
+  gulp.src(path.src.fonts).pipe(gulp.dest(path.build.fonts));
+});
+
+gulp.task("fonts:deploy", function() {
+  gulp.src(path.src.fonts).pipe(gulp.dest(path.deploy.fonts));
+});
+
+gulp.task("video:build", function() {
+  gulp.src(path.src.video).pipe(gulp.dest(path.build.video));
+});
+
+gulp.task("video:deploy", function() {
+  gulp.src(path.src.video).pipe(gulp.dest(path.deploy.video));
+});
+
+gulp.task("build", [
+  "html:build",
+  "favicon:build",
+  "js:build",
+  "js-lib:build",
+  "json:build",
+  "style:build",
+  "svg:build",
+  "clean-fonts",
+  "svg-ico:build",
+  "image:build",
+  "sprite:build",
+  "spriteSVG:build",
+  "fonts:build"
+]);
+
+gulp.task("deploy", [
+  "html:deploy",
+  "favicon:deploy",
+  "js:deploy",
+  "js-lib:deploy",
+  "json:deploy",
+  "style:deploy",
+  "fonts:deploy",
+  "svg:deploy",
+  "spriteSVG:build",
+  "svg-ico:build",
+  "image:deploy"
+]);
+
+gulp.task("watch", function() {
+  watch([path.watch.html], function(event, cb) {
+    gulp.start("html:build");
+  });
+  watch([path.watch.favicon], function(event, cb) {
+    gulp.start("favicon:build");
+  });
+  watch([path.watch.style], function(event, cb) {
+    gulp.start("style:build");
+  });
+  watch([path.watch.js], function(event, cb) {
+    gulp.start("js:build");
+  });
+  watch([path.watch.js], function(event, cb) {
+    gulp.start("js-lib:build");
+  });
+  watch([path.watch.svg], function(event, cb) {
+    gulp.start("svg:build");
+  });
+  watch([path.watch.svgico], function(event, cb) {
+    gulp.start("svg-ico:build");
+  });
+  watch([path.watch.img], function(event, cb) {
+    gulp.start("image:build");
+  });
+  watch([path.watch.sprite], function(event, cb) {
+    gulp.start("sprite:build");
+  });
+  watch([path.watch.spriteSVG], function(event, cb) {
+    gulp.start("spriteSVG:build");
+  });
+  watch([path.watch.fonts], function(event, cb) {
+    gulp.start("fonts:build");
+  });
+  watch([path.watch.video], function(event, cb) {
+    gulp.start("video:build");
+  });
+});
+
+gulp.task("webserver", function() {
+  browserSync(config);
+});
+
+gulp.task("clean", function(cb) {
+  rimraf(path.clean, cb);
+});
+
+gulp.task("clean-fonts", function(cb) {
+  rimraf("build/fonts/icons/*.*", cb);
+});
+
+gulp.task("default", ["build", "webserver", "watch"]);
--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-import objectFitImages from "object-fit-images";
-import $ from "jquery";
-import App from "./partials/app";
-
-$(document).ready(function() {
-  objectFitImages();
-  App();
-});
